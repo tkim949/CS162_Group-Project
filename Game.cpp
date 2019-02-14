b23f1aeea7bb280bb3b5b033108e68f0d5d52d92
Owl <40027596+tkim949@users.noreply.github.com>
2019-02-14 10:18:30 -0800

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -33,7 +33,7 @@ void Game::play(){
     std::cout<<"Welcome to the ants and doodle bugs' Predator-Prey simulation world"<<std::endl;
 	std::cout<<"If you input the number of days that you want to go, you can see it"<<std::endl;
 	std::cout<<"You can continue after the simulation ends, ";
-	std::cout<<"and also you can exit at that time. Now, let's begin."<<std::endl;
+	std::cout<<"and also you can stop, new start, or even exit at that time. Now, let's begin."<<std::endl;
 
     do {
 
